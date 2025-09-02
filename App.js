82556469c6a89f9879abcdd2d2b590684137b9c4
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-09-02 17:29:19 +0100

    Added a header component to the app structure.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 
 const App = () => {
     return (
         <div className="app">
+            <Header />
             <Navbar />
             <MainContent />
             <Footer />
