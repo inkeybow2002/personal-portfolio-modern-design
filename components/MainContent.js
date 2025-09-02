c3f630b485a380f38593a07c7174406e58dc173b
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-09-02 17:28:01 +0100

    feat: initial commit for components/MainContent.js

---
+++ components/MainContent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <main>
+            <section id="about">
+                <h2>About Me</h2>
+                <p>Welcome to my personal portfolio!</p>
+            </section>
+            <section id="projects">
+                <h2>Projects</h2>
+                <p>Here are some of my projects.</p>
+            </section>
+            <section id="contact">
+                <h2>Contact</h2>
+                <p>Feel free to get in touch!</p>
+            </section>
+        </main>
+    );
+};
+
+export default MainContent;
