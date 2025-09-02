363f30baa0124d07cc5c696731b7bc8a2e53513b
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-09-02 17:27:58 +0100

    feat: initial commit for components/Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>My Portfolio</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
